a82ad18994d14dc3bc5ee2aa4b15749c21bb7029
lostmart <martinpedraza1979@gmail.com>
2026-03-31 13:46:11 +0200

    fix: 'major' added to schema model

--- BACK/models/userModel.js
+++ BACK/models/userModel.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    major: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
