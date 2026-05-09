bf8f579ded3c3384369bb434abb03ca78bf3c227
Jaimie Mathangi <jaimie.mathangi78@gmail.com>
2026-05-10 01:50:48 +0200

    chore: depdendencies and gitignore configured

--- BACK/index.js
+++ BACK/index.js
@@ -13,7 +13,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static('public'))
+app.use(express.static('../FRONT'))
 
 // TODO 4: Mount studentRouter at "/api/students"
 // Hint: app.use("/api/students", studentRouter)
